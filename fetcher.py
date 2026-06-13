a1d4d204c2ce11de4f206f3b181d1a2e2225667d
Percepto <252885391+perceptreneur@users.noreply.github.com>
2026-06-12 22:53:13 -0300

    fix fetcher name, update gitignore, add .env.example

---
+++ fetcher.py
@@ -0,0 +1,18 @@
+
+
+# local imports
+import database
+
+# get youtube comments from youtbe api v3
+def fetch_youtube_comments(video_id: str):
+  
+
+
+
+def main():
+  print("[FECTHER]: alive")
+
+
+
+if __name__ == "__main__":
+  main()
